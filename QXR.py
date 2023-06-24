caf661a607b154d3eb9820f57ed263357c1ad0f1
QXR-Coders <137646996+QXR-Coders@users.noreply.github.com>
2023-06-24 23:33:34 +0500

    Update QXR.py

--- QXR.py
+++ QXR.py
@@ -1,7 +1,8 @@
 import os
+os.system('clear')
 print("[✓] FREE TRIAL ENJOY")
 os.system("xdg-open https://www.youtube.com/@MastiVloger-404")
 import os
 print("[✓] PLEASE WAIT CHACKING FOR NEW UPDATE")
 os.system(" git pull")
-import pro
+import Data
